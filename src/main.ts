f1593dc31416d4dc39cbdf024135f8ba2784639c
Alexander Samak <voprosy262@gmail.com>
2024-03-10 22:17:51 +0300

    feat: add OpenAPI file in doc folder

--- src/main.ts
+++ src/main.ts
@@ -1,11 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { NestExpressApplication } from '@nestjs/platform-express';
 import { AppModule } from './app/app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 
 const port = process.env.PORT || 4000;
 
 const bootstrap = async () => {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle('home-library')
+    .setDescription('Home Library REST Service')
+    .addTag('home-library')
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('doc', app, document);
   await app.listen(port);
   console.log(`Server started on ${port} port`);
 };
